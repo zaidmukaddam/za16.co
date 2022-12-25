175688e34cf00539228a8bd5934dfaf72a3f73c1
Zaid Mukaddam <zaidaltafmukaddam@gmail.com>
2022-12-25 23:46:02 +0530

    sb: add meet link redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 const { withPlaiceholder } = require('@plaiceholder/next')
 
-module.exports = withPlaiceholder({})
+module.exports = withPlaiceholder({
+    async redirects() {
+        return [
+            {
+                source: '/meet',
+                destination: process.env.NEXT_PUBLIC_MEET_URL,
+                permanent: true,
+            },
+        ]
+    }
+})
